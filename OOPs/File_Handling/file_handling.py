d9a8db46043f0d39d84c0afbf23a279862490588
Karan Negi <karannegi.agra@gmail.com>
2026-04-17 23:54:06 +0530

    file handling

---
+++ OOPs/File_Handling/file_handling.py
@@ -0,0 +1,26 @@
+
+# f= open("sample.txt", "w")
+# f.write("Hello world")
+# f.close() 
+
+# f=open("sample1.txt", "w")
+# f.write("Hello world")
+# f.write("\nHow  are you? ")
+# f.close()
+
+# f=open("sample.txt","w")
+# f.write("My name is Salman khan")
+# f.close()
+
+
+f=open("sample1.txt","a")
+f.write("\nI am appending this to file")
+f.close()
+
+ 
+
+
+
+
+# f= open("sample.txt","r")
+# print(f.readline())
