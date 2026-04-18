bf30a3e1b25bdcf80527def39348fb83c7e98dfb
Karan Negi <karannegi.agra@gmail.com>
2026-04-18 12:45:12 +0530

    file handling

--- OOPs/File_Handling/file_handling.py
+++ OOPs/File_Handling/file_handling.py
@@ -13,9 +13,16 @@
 # f.close()
 
 
-f=open("sample1.txt","a")
-f.write("\nI am appending this to file")
-f.close()
+# f=open("sample1.txt","a")
+# f.write("\nI am appending this to file")
+# f.close()
+
+# L=["hello\n", "hi\n", "How are you\n", " i am tuple\n", "dict\n"]
+
+
+# f= open("sample3.txt","w")
+# f.writelines(L)
+
 
  
 
@@ -24,3 +31,12 @@ f.close()
 
 # f= open("sample.txt","r")
 # print(f.readline())
+
+# with open("sample.txt", "w") as f:
+#     f.truncate(1)
+
+
+with open("sample.txt", "w") as f:
+    f.write("Virat Bhai\n")
+    f.write("Rohit Bhai")
+f.close()
