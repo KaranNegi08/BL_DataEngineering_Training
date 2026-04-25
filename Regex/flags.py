f71c9b1ccbd61d6b86d24a9ac8ac9fdaf33cd63f
Karan Negi <karannegi.agra@gmail.com>
2026-04-25 15:07:31 +0530

    flags

---
+++ Regex/flags.py
@@ -0,0 +1,21 @@
+import re
+
+# data="I love Python programming . python is awesome!"
+# pattern = r"\bpython\b"
+# match= re.findall(pattern, data, re.I)
+# print(match)
+
+# data='''
+# sachin 123
+# kk 456
+# dishant 7897'''
+# pattern = r'\d{3}$'
+# match= re.findall(pattern,data,re.M)
+# print(match)
+
+# data="hello \n world"
+# pattern= r'.'
+# match= re.findall(pattern,data, re.S)
+# print(match)
+
+
