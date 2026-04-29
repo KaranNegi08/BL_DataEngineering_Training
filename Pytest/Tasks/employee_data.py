759f2a7aa6ba1543c45f7c04bbe92aac8c53b7c8
Karan Negi <karannegi.agra@gmail.com>
2026-04-29 23:58:44 +0530

    Task 2 employee data

---
+++ Pytest/Tasks/employee_data.py
@@ -0,0 +1,23 @@
+import re
+
+def validate_id(emp_id):
+    pattern = r'^EMP-\d{4}$'
+    return re.match(pattern,emp_id, re.IGNORECASE)
+
+def validate_email(email):
+    pattern = r'^[a-zA-Z]\w+@\w+\.com$'
+    return re.match(pattern,email, re.IGNORECASE)
+
+def validate_employee(emp_id, email):
+
+    if not validate_id(emp_id):
+        raise ValueError("Invalid Emp Id...")
+
+    if not validate_email(email):
+        raise ValueError("Invalid Email...")
+
+    return True
+
+
+        
+
