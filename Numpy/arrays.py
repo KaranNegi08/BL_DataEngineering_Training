18ed7414915f6589836e1e8885b31dce2ea0c308
Karan Negi <karannegi.agra@gmail.com>
2026-05-05 11:21:41 +0530

    arrays in numpy

---
+++ Numpy/arrays.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+arr = np.array([1,2,3,4])
+arr2= np.array([[1,2,3],[4,6,5]])
+# print(arr2)
+# print(type(arr2))
+
+# arr3 = np.array([1,2,3], dtype=float)
+# print(arr3)
+
+# arr = np.arange(10,0,-2)
+# arr= np.arange(1,13).reshape(2,2,3)
+# print(arr)
+# arr[0][0][0]=100
+
+# arr = np.ones((3,4))
+# arr= np.zeros((10))
+arr= np.ones(5)*15
+
+
+# arr = np.random.random((3,4))
+
+# arr= np.random.randn(3)
+# arr= np.random.randint(1,10,size=5)
+# arr= np.random.choice((10,20,30,40), size=2)
+# np.random.seed(42)
+
+# arr = np.random.uniform(1,10, size=5)
+# arr = np.random.normal(loc=0, scale=5, size=5)
+
+# arr = np.array([10,20,30,40])
+# np.random.shuffle(arr)
+# print(arr)
+
+# import numpy as np
+
+# np.random.seed(1)
+
+# data = np.random.randint(1, 100, size=(3,3))
+# print(data)
+
+
+# arr= np.linspace(0,10,5)
+# print(arr.shape)
+
+
+# arr= np.identity(10)
+# print(arr)
+
+
+# arr= np.array([1,2,3,4,5])
+# # np.random.shuffle(arr)
+# arr2 = np.random.permutation(arr)
+# print(arr)
+# print(arr2)
+
+# ARRAY ATTRIBUTE
+# arr=  np.array([[1,2],[3,4]])
+# print(arr.ndim)
+# print(arr.shape)
+# print(arr.size)
+# print(arr.itemsize)
+
+# CHANGING DATATYPE
+# arr= np.array([[1,2],[3,4]])
+# # print(arr.dtype)
+# arr2 = arr.astype(np.int32)
+# print(arr.dtype)
+# print(id(arr))
+# print(id(arr2))
+
