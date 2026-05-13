21b1735b39a5181de063fd6fc003d93d41b1e6dd
Karan Negi <karannegi.agra@gmail.com>
2026-05-13 22:45:37 +0530

    weather monitoring system

---
+++ Pandas/Task/weather_monitoring_system.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(1)
+
+temperature_readings= np.random.randint(-4,45,size=(7,24))
+# print(temperature_readings)
+
+# Find the daily maximum and minimum temperature
+max_temp = temperature_readings.max(axis=1)
+# print(f"Daily max temperature of this week: {max_temp}")
+
+min_temp = temperature_readings.min(axis=1)
+# print(f"Daily min temperature of this week: {min_temp}")
+
+# Identify the day with the largest temperature variation
+max_variation= max_temp-min_temp
+# print(max_variation)
+# print(f"The day with the largest temperature variation is at index: {np.argmax(max_variation)}")
+
+mean_temp = temperature_readings.mean()
+std_deviation = temperature_readings.std()
+
+lower_limit = mean_temp -2 * std_deviation
+upper_limit = mean_temp +2 * std_deviation
+print(f"Mean temperature: {mean_temp}")
+# print(f"Standard deviation: {std_deviation}")
+
+outliers = (temperature_readings < lower_limit )| (temperature_readings > upper_limit)
+temperature_readings[outliers] = mean_temp
+print(f"Outliers replaced with Standard Deviation. \n {temperature_readings}")
