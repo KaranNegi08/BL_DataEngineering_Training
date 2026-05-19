b15c3060226c30ed53b6f37c9c55666eb3a8c688
Karan Negi <karannegi.agra@gmail.com>
2026-05-19 11:22:54 +0530

    path parameters and HTTP exception

---
+++ FastAPI/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Path, HTTPException
+import json
+
+app= FastAPI()
+
+
+def load_data():
+    with open("patients.json",'r') as f:
+       data=  json.load(f)
+    
+    return data
+        
+
+
+@app.get("/")
+def hello(): 
+    return {'message':'Patient management system API'}
+    # return ["karan", "rahul"]
+
+@app.get('/about')
+def about():
+    return {"message":"A fully functional API to manage your patient records"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return  data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(... , description='ID of the patient in the DB', examples='P001')):
+    data = load_data()
+
+    if patient_id in data: 
+        return data[patient_id]
+    
+    raise HTTPException(status_code=404, detail='Patient not fount')
+
+
+
+
