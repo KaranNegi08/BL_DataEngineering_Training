b558962ab5683776e173a857e0badbbd1d350d7d
Karan Negi <karannegi.agra@gmail.com>
2026-05-28 12:48:12 +0530

    Group by practice queries

---
+++ Practice/group_by.sql
@@ -0,0 +1,13 @@
+-- GROUP BY--
+
+-- SELECT dept, COUNT(*) FROM Employees GROUP BY dept;
+
+-- SELECT dept, SUM(salary) FROM Employees GROUP BY dept;
+
+-- SELECT dept, MAX(salary) FROM Employees GROUP BY dept;
+
+-- SELECT dept, AVG(salary) FROM Employees GROUP BY dept;
+
+-- SELECT dept, salary, COUNT(*) FROM Employees GROUP BY dept, salary;
+
+-- SELECT dept, AVG(salary) FROM Employees GROUP BY dept HAVING AVG(salary) > 50000;
