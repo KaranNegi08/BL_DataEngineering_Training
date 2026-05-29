c0f62eb93bb8dda3fdb41e3d250dd9610b5cf2c3
Karan Negi <karannegi.agra@gmail.com>
2026-05-29 02:38:22 -0700

    Task 6  completed

---
+++ Medical_User_Engagement_MIS/Task6.sql
@@ -0,0 +1,8 @@
+-- UPDATE patients SET city='Agra' WHERE patient_id = 5;
+-- SELECT * FROM patients;
+-- DELETE FROM appointments where current_status = 'Cancelled'; 
+-- SELECT * from appointments;
+
+-- SELECT * FROM user_engagement;
+-- INSERT INTO user_engagement (patient_id, login_count, session_duration, last_login) VALUES
+-- (2, 10, 40, '2026-06-03 05:15:00');
