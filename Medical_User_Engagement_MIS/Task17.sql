4e3ce13b54e391dddee1c1d82e6c5390b2abe60b
Karan Negi <karannegi.agra@gmail.com>
2026-05-31 12:38:27 +0530

    Task 17 of copy command completed

---
+++ Medical_User_Engagement_MIS/Task17.sql
@@ -0,0 +1,11 @@
+-- COPY patients
+-- TO 'C:/PostgreSQL_data/patients.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+
+-- SELECT * FROM patients;
+
+-- COPY patients(patient_id,fname,lname,age,gender,email,phone,city,created_at,disability)
+-- FROM 'C:/PostgreSQL_data/patients.csv'
+-- DELIMITER ','
+-- CSV HEADER;
